33650c60671195f191a9b6046978f348d058bb3b
sylvie8029 <wylian.sylvie@gmail.com>
2022-01-21 15:37:03 -0500

    server side setup

---
+++ server/src/graphql/resolvers/index.ts
@@ -0,0 +1,7 @@
+import merge from "lodash.merge";
+import { listingResolvers } from "./Listing/index";
+// import { bookingResolvers } from "./Booking";
+// import { listingResolvers } from "./Listing";
+// import { userResolvers } from "./User";
+
+export const resolvers = merge(listingResolvers);
---
+++ server/src/graphql/typeDefs.ts
@@ -0,0 +1,19 @@
+import { gql } from "apollo-server-express";
+
+export const typeDefs = gql`
+  type Listing {
+  id: ID;
+  productName: String!;
+  image: String!;
+  price: Int!;
+  rating: Int!;
+  }
+
+  type Query {
+    listings: [Listing!]!
+  }
+
+  type Mutation {
+    deleteListing(id: ID!): Listing!
+  }
+`;
