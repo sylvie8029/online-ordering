e192c5a58b4418c19bf4f5e3807ceeb1e94c5705
sylvie8029 <wylian.sylvie@gmail.com>
2022-01-20 20:41:43 -0500

    first commit

---
+++ server/src/index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+const port = 9000;
+
+app.get("/", (req, res) => res.send("hello"));
+app.listen(port);
+
+console.log(`[app] : http://localhost:${port}`);
